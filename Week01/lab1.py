e92fb4f81432e50582faa035c1a4510356a7323a
kapadya <kapadya@hotmail.com>
2026-01-29 11:18:46 -0500

    add q1, q2, q5

---
+++ Week01/lab1.py
@@ -0,0 +1,9 @@
+# Sample Coding Questions 01 Week 01
+# Kapadya Sharma
+
+# Defining variables
+#my_array = [1, 4, 7, 9]
+userAge = int(input("Please enter your age: "))
+userAge = userAge + 22
+print("Now showing shop items filtered by age: ", userAge)
+
---
+++ Week01/q5.py
@@ -0,0 +1,3 @@
+userAge = int(input("Please enter your age: "))
+userAge = userAge + 22
+print("Now showing shop items filtered by age: ", userAge)
