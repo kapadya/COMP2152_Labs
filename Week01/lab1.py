3083346352ca20f9ec82c35ee965ea5f22dcbdc8
kapadya <kapadya@hotmail.com>
2026-01-29 17:28:24 -0500

    add q1, q2, q5

--- Week01/lab1.py
+++ Week01/lab1.py
@@ -1,12 +1,21 @@
 #1. Sample Coding Questions 01 Week 01
 # Kapadya Sharma
 
-#2. Defining variables
+#2. Defining array
 my_array = [1, 4, 7, 9]
 
-#3.
+#3. Order of operations
+a = 1
+b = 2
+c = 3
+d = 4
 
-#4.
+e = (a-((b**c)//d)) + (a%c)
+print("Question 3 result: ", e)
+
+#4. Formatting
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
 
 #5.
 userAge = int(input("Please enter your age: "))
