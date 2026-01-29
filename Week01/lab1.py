247a88a7d919967f58a0d551a65d1d171fa96d65
kapadya <kapadya@hotmail.com>
2026-01-29 11:25:52 -0500

    merge q5 and lab1

--- Week01/lab1.py
+++ Week01/lab1.py
@@ -1,9 +1,14 @@
-# Sample Coding Questions 01 Week 01
+#1. Sample Coding Questions 01 Week 01
 # Kapadya Sharma
 
-# Defining variables
-#my_array = [1, 4, 7, 9]
+#2. Defining variables
+my_array = [1, 4, 7, 9]
+
+#3.
+
+#4.
+
+#5.
 userAge = int(input("Please enter your age: "))
 userAge = userAge + 22
 print("Now showing shop items filtered by age: ", userAge)
-
--- Week01/q5.py
+++
@@ -1,3 +0,0 @@
-userAge = int(input("Please enter your age: "))
-userAge = userAge + 22
-print("Now showing shop items filtered by age: ", userAge)
